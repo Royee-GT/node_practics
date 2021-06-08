c9a163f288ebbf9c361375334a014c79fae36524
LAPTOP-8EB9821T\user <roy851101@gmail.com>
2021-06-08 14:01:30 +0800

    node練習

---
+++ index.js
@@ -0,0 +1,7 @@
+var m=10;
+var n=90;
+function add(x,y){
+    return x+y;
+}
+console.log(add(m,n));
+console.log("Hello World!");
---
+++ practice.js
@@ -0,0 +1,21 @@
+// index.js
+// Include http module from Node.js
+const http = require('http')
+
+// Define server related variables
+const hostname = 'localhost'
+const port = 3000
+
+// Handle request and response here
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>YU CHIEN is the best</h1>')
+})
+
+
+
+// Start and listen the server
+server.listen(port, hostname, () => {
+  console.log(`The server is listening on http://${hostname}:${port}`)
+})
